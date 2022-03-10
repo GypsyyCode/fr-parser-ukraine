fb7b0cd00abf3aa214a1a1524160dfce78a9d947
Fabian Lehmann <fabian.lehmann@clouidcapital.de>
2022-03-10 13:36:11 +0100

    updated url

--- srv/handlers.go
+++ srv/handlers.go
@@ -18,7 +18,7 @@ type GoogleNewsFeed struct {
 func (s *Server) ParseFR() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Content.Type", "application/json")
-		url := "https://www.fr.de/politik/ukraine-konflikt-krieg-russland-kiew-kampf-invasion-hauptstadt-news-zr-91366920.html"
+		url := "https://www.fr.de/politik/ukraine-krieg-russland-kiew-invasion-kaempfe-militaer-wladimir-putin-wolodymyr-selenskyj-konflikt-news-ticker-zr-91391160.html"
 		res := parser.ParseFR(url)
 
 		if res == nil {
